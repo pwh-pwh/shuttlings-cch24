de6852ac6d1f9aa50ea9aef430100b129252dc02
pwh-pwh <plmoknpwh@163.com>
2024-12-27 17:03:24 +0800

    day16 task1

--- src/day16.rs
+++ src/day16.rs
@@ -1,19 +1,18 @@
 use std::ffi::c_long;
 use std::time::{SystemTime, UNIX_EPOCH};
-use actix_web::{post, web, HttpRequest, HttpResponse, HttpResponseBuilder, Responder};
+use actix_web::{get, post, web, HttpRequest, HttpResponse, HttpResponseBuilder, Responder};
 use actix_web::cookie::Cookie;
 use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Claims {
-    sub: String,
-    data: serde_json::Value, // Arbitrary JSON data
+    data: String, // Arbitrary JSON data
     exp: usize,
 }
 
 #[post("/16/wrap")]
-async fn wrap(data: web::Json<serde_json::Value>) -> impl Responder {
+async fn wrap(data: String) -> impl Responder {
     println!("{}", data);
     // 获取当前时间戳
     let exp = SystemTime::now()
@@ -21,7 +20,7 @@ async fn wrap(data: web::Json<serde_json::Value>) -> impl Responder {
         .unwrap()
         .as_secs() as usize
         + 3600;
-    let claims = Claims { data: data.into_inner(),exp: exp,sub: "gift".to_string() };
+    let claims = Claims { data: data.clone(),exp: exp };
     let token = encode(&Header::default(),&claims, &EncodingKey::from_secret("secret".as_ref())).unwrap();
 //     set cookie
     println!("{}", token);
@@ -29,7 +28,7 @@ async fn wrap(data: web::Json<serde_json::Value>) -> impl Responder {
 }
 
 
-#[post("/16/unwrap")]
+#[get("/16/unwrap")]
 async fn unwrap(req: HttpRequest) -> impl Responder {
     println!("unwrap");
     if let Some(cookie) = req.cookie("gift") {
@@ -37,7 +36,7 @@ async fn unwrap(req: HttpRequest) -> impl Responder {
         println!("token: {}", token);
         let result = decode::<Claims>(token, &DecodingKey::from_secret("secret".as_ref()),&Validation::default()).unwrap();
         println!("result: {}", result.claims.data);
-        HttpResponse::Ok().json(result.claims.data)
+        HttpResponse::Ok().body(result.claims.data)
     } else {
         HttpResponse::BadRequest().finish()
     }
@@ -50,11 +49,11 @@ mod tests {
 
     #[test]
     fn test_jwt() {
-        let body = "hello";
+        /*let body = "hello";
         let claims = Claims { body: body.to_string(),exp: 9999999999 };
         let token = encode(&Header::default(),&claims, &EncodingKey::from_secret("secret".as_ref())).unwrap();
         println!("token: {}", token);
         let result = decode::<Claims>(&token, &DecodingKey::from_secret("secret".as_ref()),&Validation::default()).unwrap();
-        println!("result: {}", result.claims.body);
+        println!("result: {}", result.claims.body);*/
     }
 }
